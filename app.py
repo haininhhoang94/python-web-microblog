c3c77efe55f756a3640a6e504007c454fd0d6854
Hai Ninh Hoang <34930708+haininhhoang94@users.noreply.github.com>
2021-06-28 13:11:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+# App factory
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(
+        "mongodb+srv://haininhhoang94:trangtrinh1811@microblog-application.cnt6v.mongodb.net/test?authSource=admin&replicaSet=atlas-ytu7u2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    )
+    app.db = client.microblog
+    entries = []
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"),
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
